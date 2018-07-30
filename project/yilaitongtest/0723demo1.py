e30bbb70b1e02dfa97cf46b5c11803c335eca6d2
lcscim <401143122@qq.com>
2018-07-30 18:51:34 +0800

    0730 commit

--- project/yilaitongtest/0723demo1.py
+++ project/yilaitongtest/0723demo1.py
@@ -1,38 +1,48 @@
-import urllib.request, urllib.parse, urllib.error
-import http.cookiejar
-
-LOGIN_URL = 'http://www.yilaitong.net/index.php?s=/Home/PCLogin/exam&name='
-get_url = 'http://www.yilaitong.net/index.php?s=/Home/Goods/index'  # 利用cookie请求访问另一个网址
-
-values = {'UserName': '18903827778', 'password': 'a123456'}
-postdata = urllib.parse.urlencode(values).encode()
-
-headers = {'Host': 'www.yilaitong.net',
-           'Connection': 'keep-alive',
-           'Cache-Control': 'max-age=0',
-           'Upgrade-Insecure-Requests': '1',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-           'Accept-Language': 'zh-CN,zh;q=0.9'
-           }
-
-cookie_filename = 'cookie_jar.txt'
-cookie_jar = http.cookiejar.MozillaCookieJar(cookie_filename)
-handler = urllib.request.HTTPCookieProcessor(cookie_jar)
-opener = urllib.request.build_opener(handler)
-
-request = urllib.request.Request(LOGIN_URL, postdata, headers)
-try:
-    response = opener.open(request)
-    # print(response.read().decode())
-except urllib.error.URLError as e:
-    print(e.code, ':', e.reason)
-
-cookie_jar.save(ignore_discard=True, ignore_expires=True)  # 保存cookie到cookie.txt中
-for item in cookie_jar:
-    print('Name = ' + item.name)
-    print('Value = ' + item.value)
-
-get_request = urllib.request.Request(get_url, headers=headers)
-get_response = opener.open(get_request)
-print(get_response.read().decode("utf-8"))
+import sys  
+import re  
+import urllib2  
+import urllib  
+import requests  
+import cookielib  
+  
+## 这段代码是用于解决中文报错的问题    
+reload(sys)    
+sys.setdefaultencoding("utf8")    
+#####################################################  
+#登录人人  
+loginurl = 'http://www.yilaitong.net/index.php?s=/Home/PCLogin/login.html'  
+logindomain = 'yilaitong.net'  
+  
+class Login(object):  
+      
+    def __init__(self):  
+        self.name = ''  
+        self.passwprd = ''  
+        self.domain = ''  
+  
+        self.cj = cookielib.LWPCookieJar()              
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))   
+        urllib2.install_opener(self.opener)      
+      
+    def setLoginInfo(self,username,password,domain):  
+        '''''设置用户登录信息'''  
+        self.name = username  
+        self.pwd = password  
+        self.domain = domain  
+  
+    def login(self):  
+        '''''登录网站'''  
+        loginparams = {'domain':self.domain,'UserName':self.name, 'password':self.pwd}  
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}  
+        req = urllib2.Request(loginurl, urllib.urlencode(loginparams),headers=headers)    
+        response = urllib2.urlopen(req)  
+        self.operate = self.opener.open(req)  
+        thePage = response.read()          
+          
+if __name__ == '__main__':     
+    userlogin = Login()  
+    username = '18903827778'  
+    password = 'a123456'  
+    domain = logindomain  
+    userlogin.setLoginInfo(username,password,domain)  
+    userlogin.login()  
