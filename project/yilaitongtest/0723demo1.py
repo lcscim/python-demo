ecb8d90449c2972dfdb9d42f914cb489af58090b
lcscim <401143122@qq.com>
2018-08-01 17:39:12 +0800

    0801 commit

--- project/yilaitongtest/0723demo1.py
+++ project/yilaitongtest/0723demo1.py
@@ -1,28 +1,61 @@
-from urllib import request
-from urllib import error
-from urllib import parse
-from http import cookiejar
+import requests
+from urllib.request import urlretrieve
+import os
+import openpyxl
+import yltbj
 
+def find_price(url):
+    wb = openpyxl.Workbook()
+    wb.guess_types = True
+    wb.create_sheet(index = 0, title = "华为")
+    wb.create_sheet(index = 1, title = "小米")
+    ws1 = wb['华为']
+    ws2 = wb['小米']
+    ws1['A1'] = "型号"
+    ws1['B1'] = "价格"
+    ws2['A1'] = "型号"
+    ws2['B1'] = "价格"
+    headers = {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1; MI PAD 2 MIUI/V9.6.1.0.LACCNFD)',
+               'Host': 'www.yilaitong.net',
+               'Connection':'Keep-Alive',
+               'Accept-Encoding': 'gzip'
+               }
+    heros_url1 = "https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=487"
+    heros_url2 = "https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=496"
+    req1 = requests.get(url=heros_url1, headers=headers).json()
+    name1 = []
+    price1 = []
+    for i1 in req1:
+        #print(i.get('GTitle'))
+        phonename = i1.get('GTitle').split("--")[1]+i1.get('GTitle').split("--")[2]
+        name1.append(phonename)
+        price1.append(i1.get('Price'))
+    result1 = []
+    length1 = len(name1)
+    for b1 in range(length1):
+        result1.append([name1[b1],price1[b1]])
+
+    for each1 in result1:
+        ws1.append(each1)
+
+    req2 = requests.get(url=heros_url2, headers=headers).json()
+    name2 = []
+    price2 = []
+    for i2 in req2:
+        #print(i.get('GTitle'))
+        phonename = i2.get('GTitle').split("--")[1]+i2.get('GTitle').split("--")[2]
+        name2.append(phonename)
+        price2.append(i2.get('Price'))
+    result2 = []
+    length2 = len(name2)
+    for b2 in range(length2):
+        result2.append([name2[b2],price2[b2]])
+
+    for each2 in result2:
+        ws2.append(each2)
+        
+    wb.save('测试报价.xlsx')
 if __name__ == '__main__':
-    #登陆地址
-    login_url = 'http://www.yilaitong.net/index.php'    
-    #User-Agent信息                   
-    user_agent = r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36'
-    #Headers信息
-    head = {'User-Agnet': user_agent, 'Connection': 'keep-alive'}
-    #登陆Form_Data信息
-    Login_Data = {}
-    Login_Data['UserName'] = '18903827778'       #改成你自己的用户名
-    Login_Data['password'] = 'a123456'        #改成你自己的密码
-    #使用urlencode方法转换标准格式
-    logingpostdata = parse.urlencode(Login_Data).encode('utf-8')
-    #声明一个CookieJar对象实例来保存cookie
-    cookie = cookiejar.CookieJar()
-    #利用urllib.request库的HTTPCookieProcessor对象来创建cookie处理器,也就CookieHandler
-    cookie_support = request.HTTPCookieProcessor(cookie)
-    #通过CookieHandler创建opener
-    opener = request.build_opener(cookie_support)
-    #创建Request对象
-    req1 = request.Request(url=login_url, data=logingpostdata, headers=head)
-    response1 = opener.open(req1)
-    print(response1.read().decode('utf-8'))
+    #find_price()
+    print(bj_urls.urls)
+
---
+++ project/yilaitongtest/yltbj.py
@@ -0,0 +1,5 @@
+class BjUrls:
+    urls = [{"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=435","home":"2d09","sale":"苹果"},
+            {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=487","home":"2a45","sale":"华为"},
+            {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=496","home":"2b03","sale":"小米"},
+            {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=273","home":"2d04","sale":"魅族"}]
