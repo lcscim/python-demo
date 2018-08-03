c38f89349e1a9ae6c835a5cef777bcf84fef4286
lcscim <you@example.com>
2018-08-03 22:21:30 +0800

    0803 Home commit

--- project/yilaitongtest/0723demo1.py
+++ project/yilaitongtest/0723demo1.py
@@ -16,7 +16,7 @@ def find_url(url):
     name = []
     price = []
     for i in req:
-        phonename = i.get('GTitle').split('--')[1]+' '+i.get('GTitle').split('--')[2]
+        phonename = i.get('GTitle')
         name.append(phonename)
         price.append(i.get('Price'))
 
@@ -43,19 +43,22 @@ def save_price(a):
         wb[salename]['B1'] = "价格"
         wb[salename]['A1'].style = highlight
         wb[salename]['B1'].style = highlight
-        wb[salename].column_dimensions['A'].width = 40
+        wb[salename].column_dimensions['A'].width = 50
         c = 2
         
         for each in find_url(url)[0]:
             if each[0].find('耳机') == -1:
                 if each[0].find('移动') == -1:
                     if each[0].find('电信') == -1:
-                        wb[salename].append(each)
-                        ws = wb[salename]['A{}'.format(c)]
-                        ws1 = wb[salename]['D{}'.format(c)]
-                        ws.alignment = Alignment(horizontal='center', vertical='center')
-                        ws1.alignment = Alignment(horizontal='center', vertical='center')
-                        c += 1
+                        if each[0].find('手环') == -1:
+                            wb[salename].append(each)
+                            ws = wb[salename]['A{}'.format(c)]
+                            ws1 = wb[salename]['D{}'.format(c)]
+                            ws.alignment = Alignment(horizontal='center', vertical='center')
+                            ws1.alignment = Alignment(horizontal='center', vertical='center')
+                            c += 1
+                        else:
+                            continue
                     else:
                         continue
                 else:
@@ -127,18 +130,24 @@ def checkout(sale,url):
     for each in find_url(url)[0]:
         if each[0] in name:
             num = name.index(each[0])
-            wb['{}'.format(sale)]['D{}'.format(num+2)] = (int(each[3])+int(zhi[num+2]))/2
+            if zhi[num] != 0: 
+                wb['{}'.format(sale)]['D{}'.format(num+2)] = (int(each[3])+int(zhi[num]))/2
+            else:
+                wb['{}'.format(sale)]['D{}'.format(num+2)] = each[3]
         else:
             
             if each[0].find('耳机') == -1:
                 if each[0].find('移动') == -1:
                     if each[0].find('电信') == -1:
-                        wb[sale].append(each)
-                        ws = wb[sale]['A{}'.format(c)]
-                        ws1 = wb[sale]['D{}'.format(c)]
-                        ws.alignment = Alignment(horizontal='center', vertical='center')
-                        ws1.alignment = Alignment(horizontal='center', vertical='center')
-                        c += 1
+                        if each[0].find('手环') == -1:
+                            wb[sale].append(each)
+                            ws = wb[sale]['A{}'.format(c)]
+                            ws1 = wb[sale]['D{}'.format(c)]
+                            ws.alignment = Alignment(horizontal='center', vertical='center')
+                            ws1.alignment = Alignment(horizontal='center', vertical='center')
+                            c += 1
+                        else:
+                            continue
                     else:
                         continue
                 else:
@@ -148,23 +157,75 @@ def checkout(sale,url):
 
 
     wb.save('{}报价.xlsx'.format(datetime.date.today()))
-    
+
+def zong_price():
+    pinpai = ['苹果','华为','小米','魅族']
+    wb = openpyxl.load_workbook('{}报价.xlsx'.format(datetime.date.today()))
+    wb.guess_types = True
+    wb.create_sheet(index = len(pinpai), title = '总报价')
+    for i in range(len(pinpai)):
+        a = []
+        b = []
+        rows = wb['{}'.format(pinpai[i])].max_row
+        wsa = wb[pinpai[i]]['A1:A{}'.format(rows)]
+        wsb = wb[pinpai[i]]['B1:B{}'.format(rows)]
+        for j in wsa:
+            for each in j:
+                a.append(each.value)
+        for k in wsb:
+            for each1 in k:
+                b.append(each1.value)
+        if i == 0:
+            for x in range(rows):
+                
+                wb['总报价']['A{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价']['B{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价'].column_dimensions['A'].width = 40
+                wb['总报价']['A{}'.format(x+1)] = a[x]
+                wb['总报价']['B{}'.format(x+1)] = b[x]
+        if i == 1:
+            for x in range(rows):
+                
+                wb['总报价']['C{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价']['D{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价'].column_dimensions['C'].width = 40
+                wb['总报价']['C{}'.format(x+1)] = a[x]
+                wb['总报价']['D{}'.format(x+1)] = b[x]
+        if i == 2:
+            for x in range(rows):
+                
+                wb['总报价']['E{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价']['F{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价'].column_dimensions['E'].width = 40
+                wb['总报价']['E{}'.format(x+1)] = a[x]
+                wb['总报价']['F{}'.format(x+1)] = b[x]
+        if i == 3:
+            for x in range(rows):
+                
+                wb['总报价']['G{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价']['H{}'.format(x+1)].alignment = Alignment(horizontal='center', vertical='center')
+                wb['总报价'].column_dimensions['G'].width = 40
+                wb['总报价']['G{}'.format(x+1)] = a[x]
+                wb['总报价']['H{}'.format(x+1)] = b[x]
+                    
+    wb.save('{}报价.xlsx'.format(datetime.date.today()))
+           
 if __name__ == '__main__':
+    
     save_price(yltbj.bjurls())
 
-    #'''
     for j in range(len(yltbj.jybj())):
         jy_sale = yltbj.jybj()[j].get('sale')
         jy_url = yltbj.jybj()[j].get('url')
         checkout(jy_sale,jy_url)
         get_new_num(jy_sale)
-    #'''
-    #'''
+    
     for i in range(len(yltbj.bjurls())):
         sale = yltbj.bjurls()[i].get('sale')
         get_new_num(sale)
-    #'''
-
+    
+    zong_price()
+    
 
 
     
