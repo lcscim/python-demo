02e59581b244622e4e9d923c0e76475c311e4bc8
lcscim <401143122@qq.com>
2018-08-03 18:54:17 +0800

    0803 commit

--- project/yilaitongtest/0723demo1.py
+++ project/yilaitongtest/0723demo1.py
@@ -16,7 +16,7 @@ def find_url(url):
     name = []
     price = []
     for i in req:
-        phonename = i.get('GTitle').split("--")[1]+' '+i.get('GTitle').split("--")[2]
+        phonename = i.get('GTitle').split('--')[1]+' '+i.get('GTitle').split('--')[2]
         name.append(phonename)
         price.append(i.get('Price'))
 
@@ -38,13 +38,12 @@ def save_price(a):
     for i in range(len(a)):
         salename = a[i].get('sale')
         url = a[i].get('url')
-        sale = a[i].get('sale')
         wb.create_sheet(index = i, title = salename)
-        wb[salename]['A1'] = "{}型号".format(sale)
+        wb[salename]['A1'] = "{}型号".format(salename)
         wb[salename]['B1'] = "价格"
         wb[salename]['A1'].style = highlight
         wb[salename]['B1'].style = highlight
-        wb[salename].column_dimensions['A'].width = 30
+        wb[salename].column_dimensions['A'].width = 40
         c = 2
         
         for each in find_url(url)[0]:
@@ -72,7 +71,6 @@ def save_price(a):
 
 def get_new_num(sale):
     wb = openpyxl.load_workbook('{}报价.xlsx'.format(datetime.date.today()))
-    
     rows = wb['{}'.format(sale)].max_row
     ws = wb['{}'.format(sale)]['D2:D{}'.format(rows)]
     newnums = []
@@ -94,18 +92,80 @@ def get_new_num(sale):
         c = 150
     for each in ws:
         for each_cell in each:
-            if each_cell.value < a:
-                newnums.append(each_cell.value + b)
+            
+            if int(each_cell.value) < a:
+                if each_cell.value == 0:
+                    newnums.append('')
+                else:
+                    newnums.append(int(each_cell.value) + b)
             else:
-                newnums.append(each_cell.value + c)
+                newnums.append(int(each_cell.value) + c)
     
     for j in range(rows-1):
         wb['{}'.format(sale)]['B{}'.format(j+2)] = newnums[j]
         
     wb.save('{}报价.xlsx'.format(datetime.date.today()))
+
+def checkout(sale,url):
+    
+    wb = openpyxl.load_workbook('{}报价.xlsx'.format(datetime.date.today()))
+    wb.guess_types = True
+    rows = wb['{}'.format(sale)].max_row
+    wsa = wb['{}'.format(sale)]['A2:A{}'.format(rows)]
+    wsd = wb['{}'.format(sale)]['D2:D{}'.format(rows)]
+
+    name = []
+    zhi = []
+    for each in wsa:
+        for each_cell in each:
+            name.append(each_cell.value)
+    for each1 in wsd:
+        for each_cell1 in each1:
+            zhi.append(each_cell1.value)
+    c = rows + 1
+    
+    for each in find_url(url)[0]:
+        if each[0] in name:
+            num = name.index(each[0])
+            wb['{}'.format(sale)]['D{}'.format(num+2)] = (int(each[3])+int(zhi[num+2]))/2
+        else:
+            
+            if each[0].find('耳机') == -1:
+                if each[0].find('移动') == -1:
+                    if each[0].find('电信') == -1:
+                        wb[sale].append(each)
+                        ws = wb[sale]['A{}'.format(c)]
+                        ws1 = wb[sale]['D{}'.format(c)]
+                        ws.alignment = Alignment(horizontal='center', vertical='center')
+                        ws1.alignment = Alignment(horizontal='center', vertical='center')
+                        c += 1
+                    else:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+
+
+    wb.save('{}报价.xlsx'.format(datetime.date.today()))
+    
 if __name__ == '__main__':
     save_price(yltbj.bjurls())
+
+    #'''
+    for j in range(len(yltbj.jybj())):
+        jy_sale = yltbj.jybj()[j].get('sale')
+        jy_url = yltbj.jybj()[j].get('url')
+        checkout(jy_sale,jy_url)
+        get_new_num(jy_sale)
+    #'''
+    #'''
     for i in range(len(yltbj.bjurls())):
         sale = yltbj.bjurls()[i].get('sale')
         get_new_num(sale)
+    #'''
 
+
+
+    
+        
--- project/yilaitongtest/yltbj.py
+++ project/yilaitongtest/yltbj.py
@@ -3,4 +3,15 @@ def bjurls():
             {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=487","home":"2a45","sale":"华为"},
             {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=496","home":"2b03","sale":"小米"},
             {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=273","home":"2d04","sale":"魅族"}]
+            
     return urls
+def jybj():
+    otherurls = [{"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=228","home":"3a33","sale":"苹果"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=483","home":"2a26","sale":"小米"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=283","home":"1c32","sale":"华为"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=3054","home":"1d09","sale":"华为"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=420","home":"1c20","sale":"华为"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=353","home":"2a16","sale":"魅族"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=212","home":"2c46","sale":"华为"},
+                 {"url":"https://www.yilaitong.net/?m=Android&c=Supplier&a=shopSupplier&sid=183","home":"1d13","sale":"小米"}]
+    return otherurls
