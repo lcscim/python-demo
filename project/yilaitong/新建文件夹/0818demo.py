d19cc39a7ead43ccca037f92893d0a1a52deb860
lcscim <you@example.com>
2018-08-19 23:38:26 +0800

    0819 Home commit

--- project/yilaitong/新建文件夹/0818demo.py
+++ project/yilaitong/新建文件夹/0818demo.py
@@ -4,24 +4,8 @@ from openpyxl.styles import Font
 from tkinter import *
 import tkinter.filedialog
 import warnings
-# warnings谨慎使用
-warnings.filterwarnings("ignore")
 
-root = tkinter.Tk()
-default_dir = r"C:\Users\asus\Desktop"
-fname = tkinter.filedialog.askopenfilename(title=u"选择文件",initialdir=(os.path.expanduser(default_dir)))
-
-wb = openpyxl.load_workbook('{}'.format(fname))
-root.withdraw()
-ws = wb['订单列表']
-rows = ws.max_row
-names = []
-for each in ws['C2:C{}'.format(rows-1)]:
-    for each_cell in each:
-        if each_cell.value not in names:
-            names.append(each_cell.value)
-
-def geshi(name,i):
+def geshi(ws,name,i):
     wb.create_sheet(index = i+1,title = name)
     wb[name].guess_types = True
     wb[name]['A1'] = '商品名称'
@@ -39,45 +23,86 @@ def geshi(name,i):
     wb[name].column_dimensions['D'].width = 15
     j = 2
     for a in range(rows-2):
-        
-        if names[i] == ws['C{}'.format(a+2)].value:
-            
-            wb[name]['A{}'.format(j)] = ws['C{}'.format(a+2)].value
-            wb[name]['B{}'.format(j)] = ws['E{}'.format(a+2)].value
-            if ws['K{}'.format(a+2)].value != '':
+        name1 = '{}'.format(ws['C{}'.format(a+2)].value)
+        if names[i] == name1:
+            num = ws['E{}'.format(a+2)].value
+            if int(num) < 2:
                 
-                wb[name]['C{}'.format(j)] = ws['K{}'.format(a+2)].value
-                wb[name]['D{}'.format(j)] = ws['L{}'.format(a+2)].value
-                wb[name]['E{}'.format(j)] = ws['M{}'.format(a+2)].value
+                wb[name]['A{}'.format(j)] = ws['C{}'.format(a+2)].value
+                wb[name]['B{}'.format(j)] = ws['E{}'.format(a+2)].value
+                name2 = '{}'.format(ws['K{}'.format(a+2)].value)
+                if name2 == 'None':
+                    for k in range(1,30):
+                        name3 = '{}'.format(ws['K{}'.format(a+2-k)].value)
+                        if name3 == 'None':
+                            continue
+                        else:
+                            wb[name]['C{}'.format(j)] = ws['K{}'.format(a+2-k)].value
+                            wb[name]['D{}'.format(j)] = ws['L{}'.format(a+2-k)].value
+                            wb[name]['E{}'.format(j)] = ws['M{}'.format(a+2-k)].value
+                            break
+                else:
+                    wb[name]['C{}'.format(j)] = ws['K{}'.format(a+2)].value
+                    wb[name]['D{}'.format(j)] = ws['L{}'.format(a+2)].value
+                    wb[name]['E{}'.format(j)] = ws['M{}'.format(a+2)].value
+                j+=1
             else:
-                k = 1
-                while True:
-                    if ws['K{}'.format(a+2-k)].value != '':
-                        wb[name]['C{}'.format(j)] = ws['K{}'.format(a+2-k)].value
-                        wb[name]['D{}'.format(j)] = ws['L{}'.format(a+2-k)].value
-                        wb[name]['E{}'.format(j)] = ws['M{}'.format(a+2-k)].value
-                        break
+                for b in range(int(num)):
+                    wb[name]['A{}'.format(j+b)] = ws['C{}'.format(a+2)].value
+                    wb[name]['B{}'.format(j+b)] = '1'
+                    name2 = '{}'.format(ws['K{}'.format(a+2)].value)
+                    if name2 == 'None':
+                        for k in range(1,30):
+                            name3 = '{}'.format(ws['K{}'.format(a+2-k)].value)
+                            if name3 == 'None':
+                                continue
+                            else:
+                                wb[name]['C{}'.format(j+b)] = ws['K{}'.format(a+2-k)].value
+                                wb[name]['D{}'.format(j+b)] = ws['L{}'.format(a+2-k)].value
+                                wb[name]['E{}'.format(j+b)] = ws['M{}'.format(a+2-k)].value
+                                break
                     else:
-                        k += 1
-            j+=1
+                        wb[name]['C{}'.format(j+b)] = ws['K{}'.format(a+2)].value
+                        wb[name]['D{}'.format(j+b)] = ws['L{}'.format(a+2)].value
+                        wb[name]['E{}'.format(j+b)] = ws['M{}'.format(a+2)].value
+                j += int(num)
+
+            
         else:
             continue
     wb.save('{}'.format(fname))
 
-for i in range(len(names)):
-    if len(names[i]) < 30:
-        name = names[i]
-        if len(wb.sheetnames) <= len(names)+1:
-            geshi(name,i)
+if __name__ == '__main__':
+    # warnings谨慎使用
+    warnings.filterwarnings("ignore")
+    root = tkinter.Tk()
+    default_dir = r"{}".format(os.getcwd())
+    fname = tkinter.filedialog.askopenfilename(title=u"选择文件",initialdir=(os.path.expanduser(default_dir)))
+    wb = openpyxl.load_workbook('{}'.format(fname))
+    root.withdraw()
+    ws = wb['订单列表']
+    rows = ws.max_row
+    names = []
+    for each in ws['C2:C{}'.format(rows-1)]:
+        for each_cell in each:
+            if each_cell.value not in names:
+                names.append(each_cell.value)
+    for i in range(len(names)):
+        if len(names[i]) < 30:
+            name = names[i]
+            if len(wb.sheetnames) <= len(names)+1:
+                geshi(ws,name,i)
+            else:
+                print('已录入')
+            
         else:
-            print('已录入')
-    else:
-        name = names[i][:20]+' '+names[i][30:]
-        if len(wb.sheetnames) <= len(names)+1:
-            geshi(name,i)
-        else:
-            print('已录入')
-    
+            name = names[i][:20]+' '+names[i][30:]
+            if len(wb.sheetnames) <= len(names)+1:
+                geshi(ws,name,i)
+            else:
+                print('已录入')
+
+
 '''
 a = [{'供应商':'张三','商品':['黑贝南瓜 39.9元5斤包邮','红蜜薯5斤39元包邮 48小时内发货']},
      {'供应商':'张三','商品':['黑贝南瓜 39.9元5斤包邮','红蜜薯5斤39元包邮 48小时内发货']}]
